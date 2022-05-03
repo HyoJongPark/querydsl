26d1aa5ab4c919e2c22e985369242ac7b82bacc9
rio <75190035+HyoJongPark@users.noreply.github.com>
2022-05-03 19:22:04 +0900

    데이터베이스 설정
    H2 Database 연결 설정
    예제 도메인 모델 설정 및 테스트

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -22,6 +23,7 @@ class QuerydslApplicationTests {
 	EntityManager em;
 
 	@Test
+	@Rollback(value = false)
 	void contextLoads() {
 		Hello hello = new Hello();
 		em.persist(hello);
@@ -36,5 +38,4 @@ class QuerydslApplicationTests {
 		assertThat(result).isEqualTo(hello);
 		assertThat(result.getId()).isEqualTo(hello.getId());
 	}
-
 }
